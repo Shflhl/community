1b565b8329641365290c311f5cf556e6e015594c
DSH <1424246900@qq.com>
2020-04-29 10:44:01 +0800

    init

---
+++ note.md
@@ -0,0 +1,12 @@
+# April 29
+IDEA快捷键
+ - alt + enter 导入类
+ - ctrl + Y 删除一行
+
+@SpringBootApplication添加在启动类当中相当于添加下面的注解
+- @Configuration:把当前类作为bean定义源
+- @EnableAutoConfiguration:
+- @ComponentScan:扫描当前类下面的注解 
+
+window命令
+- dir 相当于 ls
---
+++ src/main/java/life/community/Controller/GreetingController.java
@@ -0,0 +1,17 @@
+package life.community.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting") //@RequestParam中的name是表示将要查询一个名字为“name”的参数，并且把值放到后面跟着的参数中去
+    public String greeting(@RequestParam(name = "name",required = false,defaultValue = "World!")
+                           String name, Model model){
+        model.addAttribute("name",name);
+        return "greeting";
+    }
+}
