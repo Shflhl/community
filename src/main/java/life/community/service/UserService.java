d015c87b10f9511d440a49492bbd4b0c686a1b07
dsh <1424246900@qq.com>
2020-05-07 15:28:12 +0800

    修正资源访问不到的问题

---
+++ src/main/java/life/community/service/UserService.java
@@ -0,0 +1,32 @@
+package life.community.service;
+
+import life.community.mapper.UserMapper;
+import life.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+    @Autowired(required = false)
+    private UserMapper userMapper;
+
+
+    public void createOrUpdate(User user) {
+        User dbUser = userMapper.findByAccountId(user.getAccountId());
+        if (dbUser == null){
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+        }else{
+            dbUser.setGmtModified(System.currentTimeMillis());
+            dbUser.setAvatarUrl(user.getAvatarUrl());
+            dbUser.setName(user.getName());
+            dbUser.setToken(user.getToken());
+            userMapper.update(dbUser);
+        }
+    }
+}
+
+
